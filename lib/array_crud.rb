4e9826ae7f0f71582b4fded91a750941d56ad613
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-01 11:16:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["Lauren", "Text", "Apples"]
+  array = ["Lauren", "Text", "Apples", "People"]
 end
 
 def add_element_to_end_of_array(array, element)
